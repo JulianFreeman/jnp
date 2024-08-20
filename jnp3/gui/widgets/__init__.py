612009c745e46c2f6e0fcfbf902ba072de1abfa6
Julian Freeman <freemanjulian372@gmail.com>
2024-08-20 00:19:54 -0400

    v0.3.6 bugfix
    修复可选依赖未安装时报错的问题

--- jnp3/gui/widgets/__init__.py
+++ jnp3/gui/widgets/__init__.py
@@ -4,9 +4,14 @@ from .lines import HorizontalLine, VerticalLine
 from .StyleComboBox import StyleComboBox
 from .cards import Card, CardsArea
 from .DebugOutputButton import DebugOutputButton
-from .CheckUpdateButton import CheckUpdateButton
 
 
 __all__ = ["PushButtonWithItem", "HorizontalLine", "VerticalLine", "StyleComboBox",
-           "Card", "CardsArea", "IconPushButton", "DebugOutputButton",
-           "CheckUpdateButton"]
+           "Card", "CardsArea", "IconPushButton", "DebugOutputButton"]
+
+try:
+    from .CheckUpdateButton import CheckUpdateButton
+except ImportError:
+    pass
+else:
+    __all__ += ["CheckUpdateButton"]
